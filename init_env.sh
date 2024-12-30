36aa31a2af1d4c09e66a9916fe6e7e943cd7df24
R0k1e <wanghaoyu666@bupt.edu.cn>
2024-12-30 16:50:34 +0800

    support daemon process to restart slurm job

---
+++ init_env.sh
@@ -0,0 +1,2 @@
+conda activate gunicorn
+source ./scripts/run_docker_daemon.sh
--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 初始化变量
-sbatch_nodes=("g3006" "g3007")
+sbatch_nodes=("g3005" "g3006" "g3007" "g3008")
 HF_MODEL_NAME="/data/public/wangshuo/LongContext/model/Qwen/Qwen2.5-72B-Instruct-AWQ-YARN-128k"
 INFER_TYPE="vLLM"
 PER_PROC_GPUS=2
@@ -11,7 +11,7 @@ JOB_NAME="vllm_job"
 NGINX_CONF="./nginx_docker/nginx/nginx.conf"
 node_port=$((PORT + 1))
 CHECK_INTERVAL=10  # 检查间隔时间（秒）
-MAX_FAILURES=3     # 最大失败次数
+MAX_FAILURES=6     # 最大失败次数
 
 # 创建日志目录
 mkdir -p ./log
@@ -46,8 +46,8 @@ EOL
             proxy_set_header X-Real-IP \$remote_addr;
             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Proto \$scheme;
-            proxy_read_timeout 300s;
-            proxy_connect_timeout 300s;
+            proxy_read_timeout 600s;
+            proxy_connect_timeout 600s;
         }
     }
 }
@@ -56,10 +56,10 @@ EOL
     echo "Nginx configuration file generated at $NGINX_CONF"
 }
 
-# 提交 Slurm 任务
+# 提交 Slurm 任务并获取 jobid
 submit_slurm_job() {
     local node=$1
-    sbatch --job-name=$JOB_NAME \
+    jobid=$(sbatch --job-name=$JOB_NAME \
         --partition=xl \
         --nodes=1 \
         --nodelist="$node" \
@@ -67,8 +67,9 @@ submit_slurm_job() {
         --gres=gpu:8 \
         --cpus-per-task=8 \
         --output="./log/job_$node.log" \
-        --wrap="bash ./scripts/run_vllm.sh $HF_MODEL_NAME $INFER_TYPE $PER_PROC_GPUS $node_port $ENV_NAME" &
-    echo "vLLM backend starting on $node"
+        --wrap="bash ./scripts/run_vllm.sh $HF_MODEL_NAME $INFER_TYPE $PER_PROC_GPUS $node_port $ENV_NAME" | awk '{print $4}')
+    echo "vLLM backend starting on $node with jobid $jobid"
+    echo $jobid > "./log/jobid_$node.log"
 }
 
 submit_slurm_jobs() {
@@ -121,18 +122,21 @@ check_upstream() {
         if [ "${FAILURE_COUNT[$server]}" -ge "$MAX_FAILURES" ]; then
             echo "Server $server has failed $MAX_FAILURES times, restarting..."
             node=$(echo $server | cut -d':' -f1)
+            if [ -f "./log/jobid_$node.log" ]; then
+                old_jobid=$(cat "./log/jobid_$node.log")
+                scancel $old_jobid
+            fi
             submit_slurm_job $node
             FAILURE_COUNT[$server]=0
         fi
     done
 }
-sleep 
 
 while true; do
     echo -ne "\033[2J\033[H"
     check_upstream
     for ((i=CHECK_INTERVAL; i>0; i--)); do
-        echo -ne "Current time: $(date) | Sleeping for $i seconds... | Ctrl C to exit\r"
+        echo -ne "$(date), Sleeping for $i seconds... \t| Ctrl C to exit\r"
         sleep 1
     done
 done
